2aef8e807df0a5359f1d5aff0c37b46e08b211a5
Henrique <henrique.krastins@gmail.com>
2022-12-12 22:57:25 -0300

    feat: implemented tests for login and signup

--- tests/factories/users-factory.ts
+++ tests/factories/users-factory.ts
@@ -1,1 +1,15 @@
 import { prisma } from '../../src/config/database';
+import { faker } from '@faker-js/faker';
+
+export async function createValidUser() {
+  return {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
+export async function createInvalidUser() {
+  return {
+    invalid: faker.internet.userName(),
+  };
+}
--- tests/integration/registration.test.ts
+++ tests/integration/registration.test.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest';
 import server, * as serverConfig from '../../src/server';
 import { cleanDb } from '../helpers';
+import * as userFactory from '../factories/users-factory';
+import httpStatus from 'http-status';
 
 beforeAll(async () => {
   await serverConfig.init();
@@ -15,21 +17,58 @@ const app = supertest(server);
 
 //AUTH
 describe('POST: /signup', () => {
-  it('If the body is invalid should return status code 400', () => {});
+  it('If the body is invalid should return status code 400', async () => {
+    const body = userFactory.createInvalidUser();
+    const response = await app.post('/signup').send(body);
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  });
 
   describe('If the body is valid:', () => {
-    it('If the user already exists should return status code 400', () => {});
+    it('If the user already exists should return status code 400', async () => {
+      const body = userFactory.createValidUser();
+      const response = await app.post('/signup').send(body);
 
-    it('Should create new user and return status code 201', () => {});
+      expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+
+    it('Should create new user and return status code 201', async () => {
+      const body = userFactory.createValidUser();
+      const response = await app.post('/signup').send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+    });
+
+    //
   });
 });
 
 describe('POST: /login', () => {
-  it('If the body is invalid should return status code 400', () => {});
+  it('If the body is invalid should return status code 400', async () => {
+    const body = userFactory.createInvalidUser();
+    const response = await app.post('/login').send(body);
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  });
 
   describe('If the body is valid:', () => {
-    it('If the user does not exist should return status code 400', () => {});
+    it('If the user does not exist should return status code 400', async () => {
+      const body = userFactory.createValidUser();
+      const response = await app.post('/login').send(body);
 
-    it('Should log user and return token with status 200', () => {});
+      expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+
+    it('Should log user and return token with status 200', async () => {
+      const body = userFactory.createValidUser();
+      const response = await app.post('/login').send(body);
+
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toBe({
+        token: expect.any(String),
+      });
+    });
+
+    //
   });
 });
