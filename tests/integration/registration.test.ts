677559c428f950cafcc0ced8fca25fbc04862af9
Henrique <henrique.krastins@gmail.com>
2022-12-12 22:28:07 -0300

    First branch commit

--- tests/integration/registration.test.ts
+++ tests/integration/registration.test.ts
@@ -1,3 +1,35 @@
-describe('This is just a simple test', () => {
-  it('Should just pass', () => {});
+import supertest from 'supertest';
+import server, * as serverConfig from '../../src/server';
+import { cleanDb } from '../helpers';
+
+beforeAll(async () => {
+  await serverConfig.init();
+  await cleanDb();
+});
+
+beforeEach(async () => {
+  await cleanDb();
+});
+
+const app = supertest(server);
+
+//AUTH
+describe('POST: /signup', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user already exists should return status code 400', () => {});
+
+    it('Should create new user and return status code 201', () => {});
+  });
+});
+
+describe('POST: /login', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user does not exist should return status code 400', () => {});
+
+    it('Should log user and return token with status 200', () => {});
+  });
 });
--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -7,8 +7,58 @@ beforeAll(async () => {
   await cleanDb();
 });
 
+beforeEach(async () => {
+  await cleanDb();
+});
+
 const app = supertest(server);
 
-describe('This is just a simple test', () => {
-  it('Should just pass', () => {});
+//USER-INFO
+describe('GET: /profile', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  it('If the token is invalid should return status code 403', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user has no info saved should return satus code 204', () => {});
+
+    it('If the user has info saved should return info with status code 200', () => {});
+  });
+});
+
+describe('POST: /profile', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  it('If the token is invalid should return status code 403', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user already has info should return status code 405', () => {});
+
+    it('Should create user info and return with status 200', () => {});
+  });
+});
+
+describe('PUT: /profile', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  it('If the token is invalid should return status code 403', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user has no info should return status code 405', () => {});
+
+    it('Should update user info and return with status 200', () => {});
+  });
+});
+
+//History
+describe('GET: /history', () => {
+  it('If the body is invalid should return status code 400', () => {});
+
+  it('If the token is invalid should return status code 403', () => {});
+
+  describe('If the body is valid:', () => {
+    it('If the user has no info saved should return satus code 204', () => {});
+
+    it('If the user has info saved should return info with status code 200', () => {});
+  });
 });
